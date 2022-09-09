99c2939ca8b9ce959f1d98f5fd4747c261b1e1e0
Aaron Ouellette <aaronjo2@illinois.edu>
2022-09-09 11:54:47 -0500

    change box to [0, L)

--- alpha_complex_periodic_persistence.cpp
+++ alpha_complex_periodic_persistence.cpp
@@ -16,6 +16,9 @@ using Persistent_cohomology =
     Gudhi::persistent_cohomology::Persistent_cohomology<Simplex_tree, Gudhi::persistent_cohomology::Field_Zp>;
 
 
+// convert 2D vector of 3D positions to vector of point objects
+// input: [[x_i, y_i, z_i], ...]
+// output: [point_i, ...]
 template <typename AlphaComplex3d>
 std::vector<typename AlphaComplex3d::Point_3> coords_to_points(const std::vector<std::vector<double>> &coords) {
     using Point = typename AlphaComplex3d::Point_3;
@@ -29,6 +32,9 @@ std::vector<typename AlphaComplex3d::Point_3> coords_to_points(const std::vector
 }
 
 
+// returns 3D vector of persistence pairs
+// [dim_0, dim_1, dim_2]
+//   dim_i: [[birth_j, death_j], ...]
 std::vector<std::vector<std::vector<double>>> calc_persistence(const std::vector<std::vector<double>> &coords, int coeff_field, double min_persistence, double boxsize = 0.) {
 
     Simplex_tree simplex_tree;
@@ -36,7 +42,7 @@ std::vector<std::vector<std::vector<double>>> calc_persistence(const std::vector
     if (boxsize > 0) {
         using Alpha_complex_3d = Gudhi::alpha_complex::Alpha_complex_3d<Gudhi::alpha_complex::complexity::SAFE, false, true>;
         auto points = coords_to_points<Alpha_complex_3d>(coords);
-        Alpha_complex_3d alpha_complex(points, -boxsize, -boxsize, -boxsize, boxsize, boxsize, boxsize);
+        Alpha_complex_3d alpha_complex(points, 0, 0, 0, boxsize, boxsize, boxsize);
         alpha_complex.create_complex(simplex_tree);
     } else {
         using Alpha_complex_3d = Gudhi::alpha_complex::Alpha_complex_3d<Gudhi::alpha_complex::complexity::SAFE, false, false>;
@@ -47,6 +53,7 @@ std::vector<std::vector<std::vector<double>>> calc_persistence(const std::vector
 
     int st_dimension = simplex_tree.dimension();
 
+    // calculate persistent homology
     Persistent_cohomology pcoh(simplex_tree, true);
     pcoh.init_coefficients(coeff_field);
     pcoh.compute_persistent_cohomology(min_persistence);
@@ -58,6 +65,7 @@ std::vector<std::vector<std::vector<double>>> calc_persistence(const std::vector
         pairs_d = pcoh.intervals_in_dimension(dim);
         pairs_d_vec.clear();
         for (int i = 0; i < pairs_d.size(); i++) {
+            // take sqrt to get alpha instead of the filtration value (alpha^2)
             pairs_d_vec.push_back({sqrt(pairs_d[i].first), sqrt(pairs_d[i].second)});
         }
         persistence_pairs.push_back(pairs_d_vec);
@@ -68,13 +76,15 @@ std::vector<std::vector<std::vector<double>>> calc_persistence(const std::vector
 
 
 int main() {
-    int N_points = 10;
+    // For some reason need ~ >50 points for periodic triangulation to work
+    int N_points = 100;
     double boxsize = 1;
 
     std::default_random_engine generator;
     generator.seed(10);
-    std::uniform_real_distribution<double> distribution(-boxsize, boxsize);
+    std::uniform_real_distribution<double> distribution(0, boxsize);
 
+    // generate random points inside box
     std::vector<std::vector<double>> points;
     for (int i = 0; i < N_points; i++) {
         std::vector<double> point = {distribution(generator), distribution(generator), distribution(generator)};
@@ -86,7 +96,7 @@ int main() {
         std::cout << "\t" << points[i][0] << ", " << points[i][1] << ", " << points[i][2] << std::endl;
     }
 
-    std::vector<std::vector<std::vector<double>>> persistence_pairs = calc_persistence(points, 2, 0., boxsize);
+    std::vector<std::vector<std::vector<double>>> persistence_pairs = calc_persistence(points, 2, 0.);
 
         for (int dim = 0; dim < persistence_pairs.size(); dim++) {
         std::cout << "H" << dim << std::endl;
